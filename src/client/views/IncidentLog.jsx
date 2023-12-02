73b8a2f5b28b8272f8226559c2246cd784e21872
Jesse Chou <jessechou7@gmail.com>
2023-12-02 12:25:45 -0500

    feature: initialized incident-log.jsx page

---
+++ src/client/views/IncidentLog.jsx
@@ -0,0 +1,10 @@
+import Sidebar from "../components/Sidebar";
+
+export default function IncidentLog() {
+  return (
+    <>
+      <Sidebar/>
+      <h1>This is the Incident Log Page</h1>
+    </>
+  );
+}
