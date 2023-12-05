c2b601fb312a73f5428ba0daca5e10d46273bfa2
derekcoughlan <coughlan.derek@gmail.com>
2023-12-04 22:08:04 -0800

    refactor: adding some basic typography styling

--- src/client/views/Dashboard.jsx
+++ src/client/views/Dashboard.jsx
@@ -6,7 +6,9 @@ import '../App.css';
 
 function Dashboard() {
   const navigate = useNavigate();
-  const [name, setName] = useState('');
+  const [ipAddress, setIpAddress] = useState('127.0.0.1:60464');
+  const [clusterName, setClusterName] = useState('Test1');
+  const [name, setName] = useState('Jesse');
   const [isLoaded, setIsLoaded] = useState(false);
   fetch('/api/auth/checkauth')
     .then(res => res.json())
@@ -35,10 +37,30 @@ function Dashboard() {
   // if (isLoaded) {
     return (
       <div>
-        <Sidebar/>
-        <h1>Hello {name}, This is Notikube.</h1>
-        <button onClick={logout}>LogOut</button>
+      <h1 className="text-5xl font-extrabold dark:text-white">Notikube</h1>
+      <h2 className="text-4xl font-bold dark:text-white pt-5">{clusterName}</h2>
+      <h2 className="text-4xl font-bold dark:text-white pt-3">Current IP Address: {ipAddress}</h2>
+      <div id='metrics'>
+        <h3 className="text-3xl font-bold dark:text-white py-3">Cluster</h3>
+        <section id='cluster'>
+          <div id='cluster-row-1'className='display: inline-flex'>
+            <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=4`} width="300" height="200" frameborder="0"></iframe>
+            <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=5`} width="300" height="200" frameborder="0"></iframe>
+            <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=6`} width="300" height="200" frameborder="0"></iframe>
+          </div>
+          <div id='cluster-row-2' className='display: inline-flex'>
+            <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=9`} width="300" height="200" frameborder="0"></iframe>
+            <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=10`} width="300" height="200" frameborder="0"></iframe>
+            <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=11`} width="300" height="200" frameborder="0"></iframe>
+          </div>
+        </section>
+        <h3 className="text-3xl font-bold dark:text-white py-3">Nodes</h3>
+        <section id='nodes' className='display: inline-flex'>
+          <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=24`} width="300" height="200" frameborder="0"></iframe>
+          <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=26`} width="300" height="200" frameborder="0"></iframe>
+        </section>
       </div>
+    </div>
     );
   // }
   // else {
