487792e0d32f8849e0881974f2e68ab42d43bd64
derekcoughlan <coughlan.derek@gmail.com>
2023-12-05 22:34:57 -0800

    refactor: adding tailwind/flowbite styling to dashboard page and alert post router to server to test prometheus alert manager webhook capabilities

--- src/client/views/Dashboard.jsx
+++ src/client/views/Dashboard.jsx
@@ -6,7 +6,7 @@ import '../App.css';
 
 function Dashboard() {
   const navigate = useNavigate();
-  const [ipAddress, setIpAddress] = useState('127.0.0.1:60464');
+  const [ipAddress, setIpAddress] = useState('127.0.0.1:51984');
   const [clusterName, setClusterName] = useState('Test1');
   const [name, setName] = useState('Jesse');
   const [isLoaded, setIsLoaded] = useState(false);
@@ -37,29 +37,32 @@ function Dashboard() {
   // if (isLoaded) {
     return (
       <div>
-      <h1 className="text-5xl font-extrabold dark:text-white">Notikube</h1>
-      <h2 className="text-4xl font-bold dark:text-white pt-5">{clusterName}</h2>
-      <h2 className="text-4xl font-bold dark:text-white pt-3">Current IP Address: {ipAddress}</h2>
-      <div id='metrics'>
-        <h3 className="text-3xl font-bold dark:text-white py-3">Cluster</h3>
-        <section id='cluster'>
-          <div id='cluster-row-1'className='display: inline-flex'>
-            <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=4`} width="300" height="200" frameborder="0"></iframe>
-            <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=5`} width="300" height="200" frameborder="0"></iframe>
-            <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=6`} width="300" height="200" frameborder="0"></iframe>
+        <Sidebar/>
+        <div className='p-4 sm:ml-64'>
+          <h1 className="text-left pl-8 py-5 text-5xl font-extrabold dark:text-white">Dashboard</h1>
+          <h2 className="">{clusterName}</h2>
+          <h2 className="">Current IP Address: {ipAddress}</h2>
+          <div id='metrics'>
+            <h3 className="text-left pl-8 text-3xl font-bold dark:text-white py-3">Cluster</h3>
+            <section id='cluster'>
+              <div id='cluster-row-1'className='display: inline-flex'>
+                <iframe className="rounded-lg m-3" src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=4`} width="350" height="250" frameborder="0"></iframe>
+                <iframe className="rounded-lg m-3" src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=5`} width="350" height="250" frameborder="0"></iframe>
+                <iframe className="rounded-lg m-3" src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=6`} width="350" height="250" frameborder="0"></iframe>
+              </div>
+              <div id='cluster-row-2' className='display: inline-flex'>
+                <iframe className="rounded-lg m-3" src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=9`} width="350" height="250" frameborder="0"></iframe>
+                <iframe className="rounded-lg m-3" src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=10`} width="350" height="250" frameborder="0"></iframe>
+                <iframe className="rounded-lg m-3" src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=11`} width="350" height="250" frameborder="0"></iframe>
+              </div>
+           </section>
+           <h3 className="text-left pl-8 text-3xl font-bold dark:text-white py-3">Nodes</h3>
+              <section id='nodes' className='display: inline-flex'>
+                <iframe className="rounded-lg m-3" src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=24`} width="300" height="200" frameborder="0"></iframe>
+                <iframe className="rounded-lg m-3" src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=26`} width="300" height="200" frameborder="0"></iframe>
+              </section>
           </div>
-          <div id='cluster-row-2' className='display: inline-flex'>
-            <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=9`} width="300" height="200" frameborder="0"></iframe>
-            <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=10`} width="300" height="200" frameborder="0"></iframe>
-            <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=11`} width="300" height="200" frameborder="0"></iframe>
-          </div>
-        </section>
-        <h3 className="text-3xl font-bold dark:text-white py-3">Nodes</h3>
-        <section id='nodes' className='display: inline-flex'>
-          <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=24`} width="300" height="200" frameborder="0"></iframe>
-          <iframe src={`http://${ipAddress}/d-solo/garysdevil-kube-state-metrics-v2/kube-state-metrics-v2?orgId=1&from=${Date.now() - 3600000}&to=${Date.now()}&panelId=26`} width="300" height="200" frameborder="0"></iframe>
-        </section>
-      </div>
+        </div>
     </div>
     );
   // }
