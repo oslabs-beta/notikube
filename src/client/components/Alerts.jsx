e14fdbc3f98235c26ea1d8adf1b823b236d9067e
Dane Smith <daneswildlife@gmail.com>
2023-12-07 18:58:09 -0700

    updated table with edit functionality using MUI Data Grid

--- src/client/components/Alerts.jsx
+++ src/client/components/Alerts.jsx
@@ -1,105 +1,126 @@
+import * as React from 'react';
+import {
+  DataGrid,
+  GridCellModes,
+  
+} from '@mui/x-data-grid';
+import { useState, useEffect, useCallback } from 'react';
 
-import { useEffect, useState } from "react";
-import DataTable from "react-data-table-component";
+
+
+
+
 
 function Alerts() {
 
-    const [ data, setData ] = useState([]);
-    const [ loading, setLoading ] = useState(false);
+  const [ count, setCount ] = useState(0);
+  const [ data, setData ] = useState([]);
+  const [ loading, setLoading ] = useState(false);
 
-    const columns = [
-        {
-          name: 'Timestamp',
-          selector: (row) => row.timestamp,
-          sortable: true,
-          width:'20%',
-          fixedHeader: true,
+
+  const getData = useCallback(() => {
+    setLoading(true);
+    const URL = '/api/tableData';
+    fetch(URL)
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        console.log('json', json);
+        setData(json);
+      })
+      .then((json) => console.log('data', data));
+   
+    setLoading(false);
+  },[data]);
+  
+  useEffect(() => {
+    getData();
+  },[]);
+    
+
+  const updateTable = React.useCallback(
+    async (newRow) => {
+      const updatedRow = { ...newRow };
+      updatedRow.id = newRow.timestamp;
+      fetch('/api/updateAlerts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
         },
-        {
-          name: 'Type',
-          selector: (row) => row.type,
-          sortable: true,
+        body: JSON.stringify(newRow)
+      });
+      console.log('updated row', updatedRow);
+      return updatedRow;
+      },[]);
+  
+
+
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <h1 style={{ color: 'black'}}>Incident History</h1>
+      <br></br>
+      <DataGrid
+        sx={{
+        boxShadow: 2,
+        border: 2,
+        borderColor: 'black',
+        '& .MuiDataGrid-cell:hover': {
+          color: 'primary.main',
         },
-        {
-          name: 'Description',
-          selector: (row) => row.description,
-          sortable: true,
-          width:'30%',
-        },
-        {
-          name: 'Priority',
-          selector: (row) => row.priority,
-          sortable: true,
-        },
-        {
-          name: 'Status',
-          selector: (row) => row.status,
-          sortable: true,
-        },
-    ];
-
-    const customStyles = {
-        
-        headRow: {
-            style: {
-                //paddingLeft: '0px', // override the cell padding for head cells
-                paddingRight: '8px',
-                backgroundColor: 'silver',
-                fontWeight: 'bold',
-            },
-        },
-        cells: {
-            style: {
-                //paddingLeft: '20px', // override the cell padding for data cells
-                paddingRight: '8px',
-            },
-        },
-    };
-
-    const ExpandedComponent = ({ data }) => <pre>{JSON.stringify(data.description, null, 2)}</pre>;
-
-
-
-    useEffect(() => {
-        fetchData()
-    },[]);
-
-    function fetchData() {
-      setLoading(true);
-      //const URL = 'http://jsonplaceholder.typicode.com/users'
-      const URL = '/api/tableData'
-      fetch(URL)
-        .then((res) => {
-            return res.json()
-        })
-        .then((json) => {
-            console.log('json', json)
-            setData(json)
-        })
- 
-      setLoading(false);
-    }
-
-    return (
-        <div className="Alerts">
-        <h1>NotiKube Alert History</h1>
-        <DataTable
-          columns={columns}
-          data={data}
-          progressPending={loading}
-          pagination={true}
-          fixedHeader={true}
-          customStyles={customStyles}
-          expandableRows 
-          expandableRowsComponent={ExpandedComponent}
-          selectableRows
-        />
-        </div>
-    )
-
+        color: 'black',
+        }}
+        editMode='row'
+        getRowId={(data) => data.timestamp}
+        rows={data}
+        columns={columns}
+        processRowUpdate={updateTable}
+        onProcessRowUpdateError={(() => console.log('Error processing row update'))}
+        onRowEditStop={(params) => {
+          console.log(params);
+        }}
+      />
+    </div>
+  );
 }
 
-
+const columns = [
+  { 
+    field: 'timestamp', 
+    headerName: 'Timestamp', 
+    width: 180, 
+    editable: false 
+  },
+  {
+    field: 'type',
+    headerName: 'Type',
+    type: 'string',
+    editable: true,
+    align: 'left',
+    headerAlign: 'left',
+  },
+  {
+    field: 'description',
+    headerName: 'Description',
+    type: 'string',
+    width: 180,
+    editable: true,
+  },
+  {
+    field: 'priority',
+    headerName: 'Priority',
+    type: 'string',
+    width: 220,
+    editable: true,
+  },
+  {
+    field: 'status',
+    headerName: 'Status',
+    type: 'string',
+    width: 220,
+    editable: true,
+  },
+];
 
 
 export default Alerts;
