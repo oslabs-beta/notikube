a34f787edbce80ae16d3f79d38ff6d97f64aed1e
Jesse Chou <jessechou7@gmail.com>
2024-01-03 23:00:04 -0500

    feature: initialized supabase database and added connection string

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,10 +1,16 @@
 import Sidebar from "../../_components/Sidebar";
 import Link from 'next/link';
+import sql from '../../utils/db';
 
-export default function Profile() {
+export default async function Profile() {
+
+  const users = await sql`
+    select * from users where name='jesse'
+  `
   return (
     <>
       <h1>This is the Profile Page</h1>
+      {JSON.stringify(users)}
       <Link href='/app'></Link>
     </>
   );
